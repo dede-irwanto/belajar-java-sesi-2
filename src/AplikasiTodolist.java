ccb25d14797557a2573726485671c9810af49415
dede-irwanto <id.dedeirwanto@gmail.com>
2023-07-11 14:18:45 +0800

    Update Main class

--- src/AplikasiTodolist.java
+++ src/AplikasiTodolist.java
@@ -1,13 +1,25 @@
 public class AplikasiTodolist {
     public static String[] model = new String[10];
     public static void main(String[] args) {
-
+        testShowTodolist();
     }
     /**
      * Menampilkan todo list
      */
     public static void showTodolist() {
+        for (int i = 0; i < model.length; i++) {
+            var todo = model[i];
+            var no = i + 1;
 
+            if (todo != null) {
+                System.out.println(no + ". " + todo);
+            }
+        }
+    }
+    public static void testShowTodolist() {
+        model[0] = "Belajar Java Dasar";
+        model[1] = "Belajar Java OOP";
+        showTodolist();
     }
     /**
      * Menambah todo ke list
