5102253515f3ad880706a1b60b609b869cecfd75
dede-irwanto <id.dedeirwanto@gmail.com>
2023-07-11 12:33:06 +0800

    Main class

---
+++ src/AplikasiTodolist.java
@@ -0,0 +1,45 @@
+public class AplikasiTodolist {
+    public static String[] model = new String[10];
+    public static void main(String[] args) {
+
+    }
+    /**
+     * Menampilkan todo list
+     */
+    public static void showTodolist() {
+
+    }
+    /**
+     * Menambah todo ke list
+     */
+    public static void addTodolist() {
+
+    }
+    /**
+     * Menghapus todo dari list
+     */
+    public static void removeTodolist() {
+
+    }
+
+    /**
+     * Menampilkan view todo list
+     */
+    public static void viewShowTodolist() {
+
+    }
+
+    /**
+     * Menampilkan view menambah todo list
+     */
+    public static void viewAddTodolist() {
+
+    }
+
+    /**
+     * Menampilkan view menghapus todo list
+     */
+    public static void viewRemoveTodolist() {
+
+    }
+}
